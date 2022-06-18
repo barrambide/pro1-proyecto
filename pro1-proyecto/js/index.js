7e2e1b0354ff53d0922c57c7e8b28e59f5b28638
barrambide <103907540+barrambide@users.noreply.github.com>
2022-06-18 03:04:38 -0300

    Quinto punto de Control
    Hice un poco mas completa la pagina. 
    Queda por hacer la parte de js, El form Buscador, Responsive y Tratar de perfeccionar el diseño que quedo feo esteticamente.

---
+++ pro1-proyecto/js/index.js
@@ -0,0 +1,26 @@
+// console.log("hola ") ; 
+// alert("hola de nuevo")
+
+let edad = 35;
+let precio = 135.65;
+console.log(edad , precio );
+
+let nombre = "brian y eve";
+let apellido = "gomez" ; 
+alert("los nombres del usuario son " + nombre + " y el apellido registrado es " , apellido)
+
+
+
+let aprobado = true ;
+console.log(" esta aprobado el alumno ? " + aprobado)
+
+
+  //  ESTO ES NaN let operacion = 4 * 2;
+ // console.log(operacion); 
+
+const  url = "www.facebook.com" ; 
+console.log(url);
+
+
+
+ 
