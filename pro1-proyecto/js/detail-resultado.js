0a72213fb577f6d2b7fabdd22dad460ef616d82f
barrambide <barrambide@udesa.edu.ar>
2022-06-21 21:39:34 -0300

    Update detail-resultado.js

--- pro1-proyecto/js/detail-resultado.js
+++ pro1-proyecto/js/detail-resultado.js
@@ -5,7 +5,44 @@ let resultado = qsObj.get('search');
 
 let endpoint = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${resultado}`;
 let tracksContenido = document.querySelector(".contenedorTracks")
+
 fetch(endpoint)
+  .then(function (respuesta) {
+    return respuesta.json();
+
+  })
+
+   .then(function(data){
+    console.log(data);
+ 
+    let tracks = data.data;
+
+    let textoBusqueda = document.querySelector(".textoBusqueda")
+    textoBusqueda.innerHTML =  `Resultados para : ${resultado}`
+    for (let i = 0; i < 5 ; i++) {
+      tracksContenido.innerHTML += ` <article class="articleArtistas">
+      <div class="divPadre4">
+          <div class="divHija4">
+              <span class="saltopagina1"><a href="detail-canciones.html?id=${tracks[i].album.id}" class="aLetra"><i>${tracks[i].album.type}
+                          <br><i>${tracks[i].album.title} </i></i></a></span>
+              <br>
+              <a href="detail-artistas.html"><img src="${tracks[i].album.cover}" alt="mariabecerra"
+                      height="190px"></a>
+          </div>
+      </div>
+  </article>`
+    
+     
+    }
+
+   }).catch(function(errores){
+     console.log(errores);
+
+   })
+
+
+
+   fetch(endpoint)
   .then(function (respuesta) {
     return respuesta.json();
 
@@ -38,3 +75,39 @@ fetch(endpoint)
      console.log(errores);
 
    })
+
+   fetch(endpoint)
+   .then(function (respuesta) {
+     return respuesta.json();
+ 
+   })
+ 
+    .then(function(data){
+     console.log(data);
+  
+     let tracks = data.data;
+ 
+     let textoBusqueda = document.querySelector(".textoBusqueda")
+     textoBusqueda.innerHTML =  `Resultados para : ${resultado}`
+     for (let i = 0; i < 5 ; i++) {
+       tracksContenido.innerHTML += ` <article class="articleArtistas">
+       <div class="divPadre4">
+           <div class="divHija4">
+               <span class="saltopagina1"><a href="detail-canciones.html?id=${tracks[i].artist.id}" class="aLetra"><i>${tracks[i].artist.name}
+                           <br><i>${tracks[i].artist.type} </i></i></a></span>
+               <br>
+               <a href="detail-artistas.html"><img src="${tracks[i].artist.picture}" alt="mariabecerra"
+                       height="190px"></a>
+           </div>
+       </div>
+   </article>`
+     
+      
+     }
+ 
+    }).catch(function(errores){
+      console.log(errores);
+ 
+    })
+
+   
